975fce4018c36684e1dfd33b99116796cb17d99f
Christoph Gohlke <cgohlke@uci.edu>
2018-08-31 06:39:12 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# cmapfile/setup.py
+
+import sys
+import re
+
+from setuptools import setup
+
+with open('cmapfile/cmapfile.py') as fh:
+    code = fh.read()
+
+version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
+description = re.search(r'"""(.*)\.\n', code).groups()[0]
+readme = re.search(r'[\r\n?|\n]{2}"""(.*)"""[\r\n?|\n]{2}from', code,
+                   re.MULTILINE | re.DOTALL).groups()[0]
+license = re.search(r'(# Copyright.*?[\r\n?|\n])[\r\n?|\n]+""', code,
+                    re.MULTILINE | re.DOTALL).groups()[0]
+
+readme = '\n'.join([description, '=' * len(description)]
+                   + readme.splitlines()[1:])
+license = license.replace('# ', '').replace('#', '')
+
+if 'sdist' in sys.argv:
+    with open('LICENSE', 'w') as fh:
+        fh.write(license)
+    with open('README.rst', 'w') as fh:
+        fh.write(readme)
+
+setup(
+    name='cmapfile',
+    version=version,
+    description=description,
+    long_description=readme,
+    author='Christoph Gohlke',
+    author_email='cgohlke@uci.edu',
+    url='https://www.lfd.uci.edu/~gohlke/',
+    license='BSD',
+    packages=['cmapfile'],
+    python_requires='>=2.7',
+    install_requires=[
+        'numpy>=1.13',
+        'scipy>=1.1',
+        'h5py>=2.8',
+        'tiffile>=2018.8.28',
+        'oiffile>=2018.8.28'],
+    entry_points={'console_scripts': ['cmapfile = cmapfile:main']},
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+)
