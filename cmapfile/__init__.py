53b4c055bd09a9bd0c8cd71e16e3f9c2a73b3a17
Christoph Gohlke <cgohlke@uci.edu>
2018-10-19 23:13:08 -0700

    Update cmapfile/__init__.py

--- cmapfile/__init__.py
+++ cmapfile/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # cmapfile/__init__.py
 
-from .cmapfile import __doc__, __all__, __version__, main  # noqa
-from .cmapfile import *  # noqa
+from .cmapfile import __doc__, __all__, __version__, main
+from .cmapfile import *
