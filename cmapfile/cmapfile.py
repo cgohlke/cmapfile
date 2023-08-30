b3a5e87aaa19a238d0591f217956c457e2a51455
Christoph Gohlke <cgohlke@cgohlke.com>
2023-08-30 13:03:13 -0700

    Update cmapfile/cmapfile.py

--- cmapfile/cmapfile.py
+++ cmapfile/cmapfile.py
@@ -1,6 +1,6 @@
 # cmapfile.py
 
-# Copyright (c) 2014-2022, Christoph Gohlke
+# Copyright (c) 2014-2023, Christoph Gohlke
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -42,80 +42,44 @@ visualization and analysis of molecular structures and related data.
 
 :Author: `Christoph Gohlke <https://www.cgohlke.com>`_
 :License: BSD 3-Clause
-:Version: 2022.9.29
+:Version: 2023.8.30
 
-Requirements
-------------
-
-This release has been tested with the following requirements and dependencies
-(other versions may work):
-
-- `CPython 3.8.10, 3.9.13, 3.10.7, 3.11.0rc2 <https://www.python.org>`_
-  (32-bit platforms are deprecated)
-- `Numpy 1.21.5 <https://pypi.org/project/numpy/>`_
-- `Scipy 1.8.1 <https://pypi.org/project/scipy/>`_
-- `H5py 3.7.0 <https://pypi.org/project/h5py/>`_
-- `Tifffile 2022.8.12 <https://pypi.org/project/tifffile/>`_  (optional)
-- `Oiffile 2022.2.2 <https://pypi.org/project/oiffile />`_ (optional)
-
-References
+Quickstart
 ----------
 
-1. Thomas Goddard. [Chimera-users] reading in hdf5 files in chimera.
-   https://www.cgl.ucsf.edu/pipermail/chimera-users/2008-September/003052.html
-2. UCSF Chimera, an extensible molecular modeling system.
-   https://www.cgl.ucsf.edu/chimera/
-3. Globals for Images - SimFCS. https://www.lfd.uci.edu/globals/
+Install the cmapfile package and all dependencies from the
+`Python Package Index <https://pypi.org/project/cmapfile/>`_::
 
-Examples
---------
+    python -m pip install -U cmapfile[all]
 
 Print the command line usage::
 
     python -m cmapfile --help
 
-Convert a 5D LSM file to CMAP file::
+See `Examples`_ for usage cases.
 
-    python -m cmapfile "/my data directory/large.lsm"
+Source code and support are available on
+`GitHub <https://github.com/cgohlke/cmapfile>`_.
 
-Convert all BIN files in the current directory to test.cmap. The BIN files
-are known to contain 128x128x64 samples of 16-bit integers. The CMAP file
-will store float32 maps using subsampling up to 16::
+Requirements
+------------
 
-    python -m cmapfile --shape 128,128,64 --step 1,1,2 --dtype i2
-                       --cmap test.cmap --subsample 16 --astype float32 *.bin
+This revision was tested with the following requirements and dependencies
+(other versions may work):
 
-Change the step size in the CMAP file::
-
-    python -m cmapfile --step 1,1,1.5 test.cmap
-
-Notes
------
-
-The CMAP file format according to [1]::
-
-    Example of HDF format written by Chimera (Chimera map format) follows.
-    The Chimera map file reader will allow all fields to be missing (except
-    the 3D data).
-
-    /image (group, any name allowed)
-     name "centriole" (attribute)
-     step (1.2, 1.2, 1.2) (attribute)
-     origin (-123.4, -522, 34.5) (attribute)
-     cell_angles (90.0, 90.0, 90.0) (attribute)
-     rotation_axis (0.0, 0.0, 1.0) (attribute)
-     rotation_angle 45.0 (attribute, degrees)
-     color (1.0, 1.0, 0, 1.0) (attribute, rgba 0-1 float)
-     time 5 (attribute, time series frame number)
-     channel 0 (attribute, integer for multichannel data)
-     /data (3d array of uint8 (123,542,82)) (dataset, any name allowed)
-     /data_x (3d array of uint8 (123,542,82), alternate chunk shape) (dataset)
-     /data_2 (3d array of uint8 (61,271,41)) (dataset, any name allowed)
-        subsample_spacing (2, 2, 2) (attribute)
-     (more subsampled or alternate chunkshape versions of same data)
+- `CPython <https://www.python.org>`_ 3.9.13, 3.10.11, 3.11.5, 3.12rc
+- `NumPy <https://pypi.org/project/numpy/>`_ 1.25.2
+- `Scipy <https://pypi.org/project/scipy/>`_ 1.11.2
+- `H5py <https://pypi.org/project/h5py/>`_ 3.9.0
+- `Tifffile <https://pypi.org/project/tifffile/>`_ 2023.8.30 (optional)
+- `Oiffile <https://pypi.org/project/oiffile/>`_ 2023.8.30 (optional)
 
 Revisions
 ---------
+
+2023.8.30
+
+- Drop support for Python 3.8 and numpy < 1.22 (NEP29).
 
 2022.9.29
 
@@ -147,11 +111,85 @@ Revisions
 
 - Initial release.
 
+Notes
+-----
+
+The CMAP file format according to [1]::
+
+    Example of HDF format written by Chimera (Chimera map format) follows.
+    The Chimera map file reader will allow all fields to be missing (except
+    the 3D data).
+
+    /image (group, any name allowed)
+     name "centriole" (attribute)
+     step (1.2, 1.2, 1.2) (attribute)
+     origin (-123.4, -522, 34.5) (attribute)
+     cell_angles (90.0, 90.0, 90.0) (attribute)
+     rotation_axis (0.0, 0.0, 1.0) (attribute)
+     rotation_angle 45.0 (attribute, degrees)
+     color (1.0, 1.0, 0, 1.0) (attribute, rgba 0-1 float)
+     time 5 (attribute, time series frame number)
+     channel 0 (attribute, integer for multichannel data)
+     /data (3d array of uint8 (123,542,82)) (dataset, any name allowed)
+     /data_x (3d array of uint8 (123,542,82), alternate chunk shape) (dataset)
+     /data_2 (3d array of uint8 (61,271,41)) (dataset, any name allowed)
+        subsample_spacing (2, 2, 2) (attribute)
+     (more subsampled or alternate chunkshape versions of same data)
+
+References
+----------
+
+1. Thomas Goddard. [Chimera-users] reading in hdf5 files in chimera.
+   https://www.cgl.ucsf.edu/pipermail/chimera-users/2008-September/003052.html
+2. UCSF Chimera, an extensible molecular modeling system.
+   https://www.cgl.ucsf.edu/chimera/
+3. Globals for Images - SimFCS. https://www.lfd.uci.edu/globals/
+
+Examples
+--------
+
+Convert a 5D LSM file to CMAP file::
+
+    $ python -m cmapfile "/my data directory/large.lsm"
+
+Convert all BIN files in the current directory to test.cmap. The BIN files
+are known to contain 128x128x64 samples of 16-bit integers. The CMAP file
+will store float32 maps using subsampling up to 16::
+
+    $ python -m cmapfile --shape 128,128,64 --step 1,1,2 --dtype i2 \
+                         --cmap test.cmap --subsample 16 --astype float32 *.bin
+
+Change the step size in the CMAP file::
+
+    $ python -m cmapfile --step 1,1,1.5 test.cmap
+
+Print the cmapfile script usage::
+
+    $ python -m cmapfile --help
+
+    Usage: cmapfile [options] files
+
+    Convert volume data files to Chimera MAP files.
+
+    Options:
+    --version             show program's version number and exit
+    -h, --help            show this help message and exit
+    -q, --quiet
+    --filetype=FILETYPE   type of input file(s), e.g. BIN, LSM, OIF, TIF
+    --dtype=DTYPE         type of data in BIN files. e.g. uint16
+    --shape=SHAPE         shape of data in BIN files in F order, e.g 256,256,32
+    --offset=OFFSET       number of bytes to skip at beginning of BIN files
+    --step=STEP           stepsize of data in files in F order, e.g 1.0,1.0,8.0
+    --cmap=CMAP           name of output CMAP file
+    --astype=ASTYPE       type of data in CMAP file. e.g. float32
+    --subsample=SUBSAMPLE
+                            write subsampled datasets to CMAP file
+
 """
 
 from __future__ import annotations
 
-__version__ = '2022.9.29'
+__version__ = '2023.8.30'
 
 __all__ = [
     'CmapFile',
@@ -162,34 +200,29 @@ __all__ = [
     'array2cmap',
 ]
 
-import sys
-import os
 import glob
+import os
+import sys
 import warnings
 
-import numpy
 import h5py
+import numpy
 
 try:
     from scipy.ndimage import zoom
 except ImportError:
     from ndimage import zoom
 
-from tifffile import TiffFile, transpose_axes, natural_sorted, product
-from oiffile import OifFile
-
 from typing import TYPE_CHECKING
 
+from oiffile import OifFile
+from tifffile import TiffFile, natural_sorted, product, transpose_axes
+
 if TYPE_CHECKING:
-    from typing import Any, Union, Sequence, Iterator
+    from collections.abc import Iterator, Sequence
+    from typing import Any
 
-    try:
-        from numpy.typing import ArrayLike
-    except ImportError:
-        # numpy < 1.20
-        from numpy import ndarray as ArrayLike
-
-    PathLike = Union[str, os.PathLike]
+    from numpy.typing import ArrayLike, DTypeLike
 
 
 class CmapFile(h5py.File):
@@ -208,7 +241,7 @@ class CmapFile(h5py.File):
     mapcounter: int
 
     def __init__(
-        self, filename: PathLike, /, mode: str = 'w', **kwargs
+        self, filename: str | os.PathLike[Any], /, mode: str = 'w', **kwargs
     ) -> None:
         h5py.File.__init__(self, name=filename, mode=mode, **kwargs)
         self.mapcounter = 0
@@ -228,7 +261,7 @@ class CmapFile(h5py.File):
         time: int | None = None,
         channel: int | None = None,
         symmetries=None,
-        astype: numpy.dtype = None,
+        astype: DTypeLike | None = None,
         subsample: int = 16,
         chunks: bool = True,
         compression: str | None = None,
@@ -323,13 +356,13 @@ class CmapFile(h5py.File):
             compression=compression,
         )
         # create subsampled datasets
-        for i, data in enumerate(subsamples(data, int(subsample))):
+        for i, subsampled in enumerate(subsamples(data, int(subsample))):
             sample = 2 ** (i + 1)
             if verbose:
                 print(f'{sample} ', end='', flush=True)
             dset = group.create_dataset(
                 f'data{self.mapcounter:05d}_{i + 2}',
-                data=data,
+                data=subsampled,
                 chunks=chunks,
                 compression=compression,
             )
@@ -349,12 +382,12 @@ class CmapFile(h5py.File):
 
 
 def bin2cmap(
-    binfiles: Sequence[PathLike] | str,
+    binfiles: Sequence[str | os.PathLike[Any]] | str,
     /,
     shape: tuple[int, ...],
     dtype: numpy.dtype,
     offset: int = 0,
-    cmapfile: PathLike | None = None,
+    cmapfile: str | os.PathLike[Any] | None = None,
     fail: bool = True,
     **kwargs,
 ) -> None:
@@ -418,9 +451,9 @@ def bin2cmap(
 
 
 def tif2cmap(
-    tiffiles: Sequence[PathLike],
+    tiffiles: Sequence[str | os.PathLike[Any]],
     /,
-    cmapfile: PathLike | None = None,
+    cmapfile: str | os.PathLike[Any] | None = None,
     fail: bool = True,
     **kwargs,
 ) -> None:
@@ -473,7 +506,10 @@ def tif2cmap(
 
 
 def lsm2cmap(
-    lsmfile: PathLike, /, cmapfile: PathLike | None = None, **kwargs
+    lsmfile: str | os.PathLike[Any],
+    /,
+    cmapfile: str | os.PathLike[Any] | None = None,
+    **kwargs,
 ) -> None:
     """Convert 5D TZCYX LSM file to Chimera MAP files, one per channel.
 
@@ -544,7 +580,11 @@ def lsm2cmap(
 
 
 def array2cmap(
-    data: numpy.ndarray, /, axes: str, cmapfile: PathLike, **kwargs
+    data: numpy.ndarray,
+    /,
+    axes: str,
+    cmapfile: str | os.PathLike[Any],
+    **kwargs,
 ) -> None:
     """Save numpy ndarray to Chimera MAP files, one per channel.
 
@@ -586,7 +626,10 @@ def array2cmap(
 
 
 def oif2cmap(
-    oiffile: PathLike, /, cmapfile: PathLike = None, **kwargs
+    oiffile: str | os.PathLike[Any],
+    /,
+    cmapfile: str | os.PathLike[Any] | None = None,
+    **kwargs,
 ) -> None:
     """Convert OIF or OIB files to Chimera MAP files, one per channel.
 
@@ -718,7 +761,9 @@ def parse_numbers(
         raise ValueError(f"not a '{sep}' separated list of numbers") from exc
 
 
-def parse_files(files: Sequence[PathLike], /) -> Sequence[PathLike]:
+def parse_files(
+    files: Sequence[str | os.PathLike[Any]], /
+) -> Sequence[str | os.PathLike[Any]]:
     """Return list of file names from pattern or list of file names.
 
     Parameters:
